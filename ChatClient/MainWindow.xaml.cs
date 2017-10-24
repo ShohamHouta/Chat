e676f755cc3738080755f3e8d8bcdc92b3362b17
shoham houta <shoham houta>
2017-10-24 10:29:59 +0300

    Add project files.

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using ChatInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static IChattingService server;
+        private static DuplexChannelFactory<IChattingService> _channelFactory;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _channelFactory = new DuplexChannelFactory<IChattingService>(new ClientCallback(), "ChatServiceEndPoint");
+            server = _channelFactory.CreateChannel();
+            
+        }
+
+        public void TakeMessage(string message, string userName)
+        {
+               MessagesBox.Text += userName + ": " + message + "\n";
+            MessagesBox.ScrollToEnd();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageTextBox.Text.Length == 0) {
+                return;
+            }
+
+            if (LoginButton.IsEnabled == false)
+            {
+                server.SentToAll(MessageTextBox.Text, UsernameTextbox.Text);
+                TakeMessage(MessageTextBox.Text, "You");
+                MessageTextBox.Text = "";
+            }
+            else if (LoginButton.IsEnabled ==true)
+            {
+                MessageBox.Show("Please Login First.");
+            }
+           
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            int returnValue = server.Login(UsernameTextbox.Text);
+            if (returnValue == 1)
+            {
+                MessageBox.Show("You are already logged in. Try again");
+            }
+            else if (returnValue == 0)
+            {
+
+                LoadUserList(server.GetCurrentUsers());
+                MessageBox.Show("You logged in!");
+                welcomelabel.Content = "Welcome " + UsernameTextbox.Text + "!";
+                UsernameTextbox.IsEnabled = false;
+                LoginButton.IsEnabled = false;
+            }
+        }
+
+        private void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                SendButton_Click(sender, e);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            server.Logout();
+        }
+        public void AddUserToList(string userName)
+        {
+            if (UserListBox.Items.Contains(userName))
+            {
+                return;
+            }
+            UserListBox.Items.Add(userName);
+        }
+        public void RemoveUserFromList(string userName)
+        {
+            if (UserListBox.Items.Contains(userName))
+            {
+                UserListBox.Items.Remove(userName);
+            }
+        }
+        private void LoadUserList(List<string> users)
+        {
+            foreach (var user in users)
+            {
+                AddUserToList(user);
+            }
+        }
+    }
+}
---
+++ ChatServer/ChattingService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Collections.Concurrent;
+using ChatInterfaces;
+
+namespace ChatServer
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]
+    public class ChattingService : IChattingService
+    {
+
+        public ConcurrentDictionary<string, ConnectedClient> _ConnectedClients = new ConcurrentDictionary<string, ConnectedClient>();
+
+        public int Login(string userName)
+        {
+
+            foreach (var client in _ConnectedClients)
+            {
+                if(client.Key.ToLower() == userName.ToLower())
+                {
+                    return 1;
+                }
+            }
+
+            var establishedUserConnection = OperationContext.Current.GetCallbackChannel<IClient>();
+
+            ConnectedClient newClient = new ConnectedClient
+            {
+                connection = establishedUserConnection,
+                UserName = userName
+            };
+
+            _ConnectedClients.TryAdd(userName, newClient);
+
+            UpdateHelper(0, newClient.UserName);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Clientn login: {newClient.UserName} { DateTime.Now}");
+            Console.ResetColor();
+
+            return 0;
+        }
+
+        public void Logout()
+        {
+            ConnectedClient client = GetMyClient();
+            if(client != null)
+            {
+                ConnectedClient removedclient;
+                _ConnectedClients.TryRemove(client.UserName, out removedclient);
+
+                UpdateHelper(1, removedclient.UserName);
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Clientn logoff: {removedclient.UserName} { DateTime.Now}");
+                Console.ResetColor();
+            }
+        }
+
+        public ConnectedClient GetMyClient()
+        {
+            var establishedUserConnection = OperationContext.Current.GetCallbackChannel<IClient>();
+
+            foreach (var client in _ConnectedClients)
+            {
+                if(client.Value.connection == establishedUserConnection)
+                {
+                    return client.Value;
+                }
+            }
+
+            return null;
+        }
+
+        public void SentToAll(string message, string userName)
+        {
+            foreach (var client in _ConnectedClients)
+            {
+                if (client.Key.ToLower() != userName.ToLower())
+                {
+                    client.Value.connection.GetMessage(message, userName);
+                }
+            }
+        }
+        private void UpdateHelper(int value,string userName)
+        {
+            foreach (var client in _ConnectedClients)
+            {
+                if (client.Value.UserName.ToLower() != userName.ToLower())
+                {    
+                    client.Value.connection.GetUpdate(value, userName);
+                }
+            }
+        }
+
+        public List<string> GetCurrentUsers()
+        {
+            List<string> listofusers = new List<string>();
+            foreach (var client in _ConnectedClients)
+            {
+                listofusers.Add(client.Value.UserName);
+            }
+            return listofusers;
+        }
+    }
+}
